dfd7335e9bf6da7fdc199074e480a500515df2e9
shadow <lightshadow416@gmail.com>
2024-06-14 22:49:42 +0200

    the refresh redirect error from login was fixed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,17 @@ import "./style/input.css";
 import "./App.css";
 //
 import MyContainer from "./components/MyContainer/MyContainer";
-import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { AuthenticateWithRedirectCallback, useAuth } from "@clerk/clerk-react";
 import { useLayoutEffect } from "react";
 import MyLogin from "./components/MyLogin/MyLogin";
 
 function App() {
   const { isSignedIn } = useAuth();
-  const location = useLocation();
   const navigate = useNavigate();
   // auth guardian
   useLayoutEffect(() => {
-    if (!isSignedIn && location.pathname !== "/login/") {
+    if (!isSignedIn) {
       navigate("login/");
     } else {
       navigate("/dashboard");
